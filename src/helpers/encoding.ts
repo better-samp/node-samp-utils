1966acaa4fc334ac58ad0af4466e1033d3c94dff
neverlane <naer200469@gmail.com>
2023-09-11 22:02:05 +0300

    helpers: add encodeWin1251Text

--- src/helpers/encoding.ts
+++ src/helpers/encoding.ts
@@ -3,3 +3,5 @@ import iconv from 'iconv-lite';
 
 export const decodeWin1251Buffer = (buffer: Buffer) =>
   iconv.decode(buffer, 'win1251');
+export const encodeWin1251Text = (text: string) =>
+  iconv.encode(text, 'win1251');
